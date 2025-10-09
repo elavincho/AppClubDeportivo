33a2d9d11d6bbfdc138365bd32e00314154d2d38
Valeria Thomas <thomas.valeria@gmail.com>
2025-10-09 19:56:24 -0300

    Agrego variables en Registrar socios.kt

--- app/src/main/java/com/elavincho/appclubdeportivo/RegistrarSocioActivity.kt
+++ app/src/main/java/com/elavincho/appclubdeportivo/RegistrarSocioActivity.kt
@@ -4,15 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class RegistrarSocioActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
+
+      val edTxtNombre = findViewById<TextView>(R.id.edTxtNombre)
+      val edTxtApellido = findViewById<TextView>(R.id.edTxtApellido)
+      val edTxtNroDoc = findViewById<TextView>(R.id.edTxtNroDoc)
+      val edTxtFechaNac = findViewById<TextView>(R.id.edTxtFechaNac)
+      val edTxtTelefono = findViewById<TextView>(R.id.edTxtTelefono)
+      val edTxtMail = findViewById<TextView>(R.id.edTxtMail)
+      val edTxtDireccion = findViewById<TextView>(R.id.edTxtDireccion)
+
       setContentView(R.layout.activity_registrar_socio)
       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
          val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -38,6 +50,8 @@ class RegistrarSocioActivity : AppCompatActivity() {
          val intentAptoFisico= Intent(this, AptoFisicoActivity::class.java)
          /* Por ultimo hay que llamar al método startActivity() y pasarle el intent*/
          startActivity(intentAptoFisico)
+
+        /* Toast.makeText("contexto", "complete los campos",5)*/
       }
 
 
